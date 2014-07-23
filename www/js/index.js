884bf755aed82310366644f10a6c50df574557a4
diverJohn <divers@ix.netcom.com>
2014-07-23 14:49:35 -0700

    a

--- www/js/index.js
+++ www/js/index.js
@@ -21,6 +21,8 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+    
+    
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -28,14 +30,19 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+    
+    
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	console.log("jdo: device ready");
         app.receivedEvent('deviceready');
         StartBluetooth();
     },
+    
+    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
